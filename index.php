586a3c19102f14f7344c51853561efda996605b7
Luis Felipe Alvarez <luisfeal1998@hotmail.com>
2017-11-18 13:58:25 -0600

    added header view

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
-<!DOCTYPE html>
-<html>
-  <head>
+<?php include'views/header.php'; ?>
     <title>Perritos con amor</title>
   </head>
 
---
+++ views/header.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+  <title>Perritos</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+  <link rel="stylesheet" href="../css/style.css">
+
+  <script src="https://use.fontawesome.com/976e5e17b3.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script type="text/javascript" src="../js/main.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+
+</head>
