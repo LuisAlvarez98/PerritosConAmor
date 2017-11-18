0d51baddf84b1ec783753d3171fe966542e4d434
albanaguilar <albanaguilar@gmail.com>
2017-11-18 14:33:39 -0600

    Estilos css added

--- views/header.php
+++ views/header.php
@@ -6,6 +6,8 @@
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
   <link rel="stylesheet" href="../css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Patua+One" rel="stylesheet">
+
 
   <script src="https://use.fontawesome.com/976e5e17b3.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
