5d5e4c52a5e652c6ce0b865868dcdbc10dd5163c
albanaguilar <albanaguilar@gmail.com>
2017-11-18 14:29:37 -0600

    login view added

--- views/login.php
+++ views/login.php
@@ -8,15 +8,15 @@
   <link rel="stylesheet" href="../css/style.css">
 
   <script src="https://use.fontawesome.com/976e5e17b3.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Patua+One" rel="stylesheet">
+
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script type="text/javascript" src="../js/main.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
 
 </head>
 
-  </head>
-
   <body>
-    hola
+    <h1>hola</h1>
   </body>
 </html>
